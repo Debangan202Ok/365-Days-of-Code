98849e3f34be88b545c09c6f9c4803944a851e8b
paul <98002712+Debangan202Ok@users.noreply.github.com>
2024-01-09 11:51:22 +0530

    9th day of the 365

--- 2.IGNITON/components/button.jsx
+++ 2.IGNITON/components/button.jsx
@@ -1,15 +1,17 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Btn() {
-    let [count, setCount] = useState(0);
-    function increment() {
-        setCount(count + 1);
-        console.log(count);
-    }
-    return (
-        <button onClick={increment}>Click Me:{count}</button>
-    )
+  let [count, setCount] = useState(0);
+
+  function increment() {
+    setCount((prevCount) => prevCount + 1);
+  }
+
+  useEffect(() => {
+    console.log(count); // Log the updated count immediately after the state has been updated
+  }, [count]);
+
+  return <button onClick={increment}>Click Me: {count}</button>;
 }
 
 export default Btn;
