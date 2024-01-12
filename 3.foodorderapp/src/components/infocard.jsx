ebde985b1d97d19f73ffe66c81d045dfeedda34c
paul <98002712+Debangan202Ok@users.noreply.github.com>
2024-01-12 15:42:23 +0530

    11th day 365

--- 3.foodorderapp/src/components/infocard.jsx
+++ 3.foodorderapp/src/components/infocard.jsx
@@ -1,16 +1,19 @@
 const InfoCard = () => {
   return (
-    <div>
-      <div>
+    <div className="card-container">
+      <div className="img-container">
         <img src="../../public/oil.jpg" alt="image" />
       </div>
-      <div>
+      <div className="body-container">
         <h2>Product Name</h2>
         <h4>
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione,
           ipsa?
         </h4> 
+        <div className="price-con">
         <h3>Price</h3>
+        <p>$100</p>
+        </div>
         <button>Add to Cart</button>
       </div>
     </div>
