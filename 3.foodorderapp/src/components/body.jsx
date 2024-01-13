815760a305a429f7b46d15bde5dec231102ee373
paul <98002712+Debangan202Ok@users.noreply.github.com>
2024-01-13 10:13:32 +0530

    13th day of 365days UI Improvement & props

--- 3.foodorderapp/src/components/body.jsx
+++ 3.foodorderapp/src/components/body.jsx
@@ -1,14 +1,80 @@
 import InfoCard from "./infocard";
 
 const Body = () => {
+  const Data = [
+    {
+      id: 1,
+      name: "Product Name",
+      description:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione, ipsa?",
+      price: 100,
+      image: "../../public/oil.jpg",
+    },
+    {
+      id: 2,
+      name: "Product Name",
+      description:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione, ipsa?",
+      price: 100,
+      image: "../../public/oil.jpg",
+    },
+    {
+      id: 3,
+      name: "Product Name",
+      description:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione, ipsa?",
+      price: 100,
+      image: "../../public/oil.jpg",
+    },
+    {
+      id: 4,
+      name: "Product Name",
+      description:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione, ipsa?",
+      price: 100,
+      image: "../../public/oil.jpg",
+    },
+    {
+      id: 5,
+      name: "Product Name",
+      description:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione, ipsa?",
+      price: 100,
+      image: "../../public/oil.jpg",
+    },
+    {
+      id: 6,
+      name: "Product Name",
+      description:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione, ipsa?",
+      price: 100,
+      image: "../../public/oil.jpg",
+    },
+    {
+      id: 7,
+      name: "Product Name",
+      description:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione, ipsa?",
+      price: 100,
+      image: "../../public/oil.jpg",
+    },
+  ];
+
   return (
     <div className="body">
-          <InfoCard />
-          <InfoCard />
-          <InfoCard />
+      {Data.map((i)=>{
+        return (
+          <InfoCard
+            key={i.id}
+            name={i.name}
+            description={i.description}
+            price={i.price}
+            image={i.image}
+          />
+        )
+      })}
     </div>
   );
 };
-
 
 export default Body;
--- 3.foodorderapp/src/components/infocard.jsx
+++ 3.foodorderapp/src/components/infocard.jsx
@@ -1,84 +1,22 @@
-const InfoCard = () => {
-  const Data = [
-    {
-      id: 1,
-      name: "Product Name",
-      description:
-        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione, ipsa?",
-      price: 100,
-      image: "../../public/oil.jpg",
-    },
-    {
-      id: 2,
-      name: "Product Name",
-      description:
-        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione, ipsa?",
-      price: 100,
-      image: "../../public/oil.jpg",
-    },
-    {
-      id: 3,
-      name: "Product Name",
-      description:
-        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione, ipsa?",
-      price: 100,
-      image: "../../public/oil.jpg",
-    },
-    {
-      id: 4,
-      name: "Product Name",
-      description:
-        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione, ipsa?",
-      price: 100,
-      image: "../../public/oil.jpg",
-    },
-    {
-      id: 5,
-      name: "Product Name",
-      description:
-        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione, ipsa?",
-      price: 100,
-      image: "../../public/oil.jpg",
-    },
-    {
-      id: 6,
-      name: "Product Name",
-      description:
-        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione, ipsa?",
-      price: 100,
-      image: "../../public/oil.jpg",
-    },
-    {
-      id: 7,
-      name: "Product Name",
-      description:
-        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione, ipsa?",
-      price: 100,
-      image: "../../public/oil.jpg",
-    },
-  ];
+import Oil from "../../public/oil.jpg";
 
+const InfoCard = (props) => {
+  const { id, name, description, price } = props;
   return (
-    <>
-      {Data.map((item) => {
-        return (
-          <div key={item.id} className="card-container">
-            <div className="img-container">
-              <img src="../../public/oil.jpg" alt="image" />
-            </div>
-            <div className="body-container">
-              <h2>{item.name}</h2>
-              <h4>{item.description}</h4>
-              <div className="price-con">
-                <h3>Price:</h3>
-                <p>${item.price}</p>
-              </div>
-              <button>Add to Cart</button>
-            </div>
-          </div>
-        );
-      })}
-    </>
+    <div key={id} className="card-container">
+      <div className="img-container">
+        <img src={Oil} alt="image" />
+      </div>
+      <div className="body-container">
+        <h2>{name}</h2>
+        <h4>{description}</h4>
+        <div className="price-con">
+          <h3>Price:</h3>
+          <p>${price}</p>
+        </div>
+        <button>Add to Cart</button>
+      </div>
+    </div>
   );
 };
 
