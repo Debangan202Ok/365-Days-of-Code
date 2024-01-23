208751cb1e501dc6c2662c22751b34f09944c354
paul <98002712+Debangan202Ok@users.noreply.github.com>
2024-01-23 22:38:30 +0530

    last commit for today

--- 3.foodorderapp/src/components/Cart.jsx
+++ 3.foodorderapp/src/components/Cart.jsx
@@ -6,23 +6,29 @@ class Cart extends React.Component {
     super();
     console.log("I'm constructor Method");
     this.state = {
-      count: 0,
-    };
+      jsonUserData: {
+      }
+    }
   }
 
+  componentDidMount() {
+    (async () => {
+      const githubUserData = await fetch(
+        "https://api.github.com/users/debangan202ok"
+      );
+      const jsonUserData = await githubUserData.json();
+      this.setState({jsonUserData: jsonUserData})
+    })();
+  }
 
   render() {
+    const { name,bio,login } = this.state.jsonUserData;
     console.log("I'm Render Method");
     return (
       <div>
-        <h1>Cart of Class:{this.state.count}</h1>
-        <button
-          onClick={() => {
-            this.setState({ count: this.state.count + 1 });
-          }}
-        >
-          Increase
-        </button>
+        <h1>{name}</h1>
+        <h4>{bio}</h4>
+        <h4>USER NAME: {login}</h4>
         <Items name={"Burger"} />
       </div>
     );
