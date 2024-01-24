b2f5f090248f6772c89e1cafe7f3f51252896321
paul <98002712+Debangan202Ok@users.noreply.github.com>
2024-01-24 23:49:39 +0530

    last commit of the interview day

--- 3.foodorderapp/src/components/body.jsx
+++ 3.foodorderapp/src/components/body.jsx
@@ -3,92 +3,79 @@ import InfoCard from "./infocard";
 import Skeleton from "./Shimming";
 import NotFound from "./NotFound";
 import SrchArea from "./bodysmallcom/SrchArea";
-import { API_URL } from "../utils/constants";
 import { Link } from "react-router-dom";
+import setResturantData from "../utils/hooks/setResturantData";
 
 const Body = () => {
   //states
+  console.log("Hii, i'm main body");
   const [winSize, setWinSize] = useState(window.innerWidth);
-  const [filterRes, setFilterRes] = useState([]);
-  const [inputVal, setInputVal] = useState("");
-  const [srchRes, setSrchRes] = useState([]);
-
+  // const [inputVal, setInputVal] = useState("");
+  // const [srchRes, setSrchRes] = useState([]);
+  const filterRes = setResturantData();
+  console.log(filterRes);
   //style objects
   const containerWidth = {
     width: `${Math.floor(winSize / 310) * 310}px`,
   };
 
-  //! Using useEffect with IFEs to fecth data after intial rendering
   useEffect(() => {
     window.addEventListener("resize", () => {
       setWinSize(window.innerWidth);
     });
-
-    //* IFEs -Invoke Function Expression
-    (async function () {
-      const Data1 = await fetch(
-        API_URL + "lat=12.960059122809971&lng=77.57337538383284"
-      );
-      const resData = await Data1.json();
-      const resCardData =
-        resData?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle
-          ?.restaurants;
-
-      setFilterRes(resCardData);
-      setSrchRes(resCardData);
-    })();
+    console.log("voila rerender done");
   }, []);
 
   //element functions
-  const srchVal = () => {
-    const filterData = filterRes.filter((item) =>
-      item.info.name.toLowerCase().includes(inputVal.toLowerCase())
-    );
-    setSrchRes(filterData);
-  };
-
-  const fetchMoreBtn = async () => {
-    //* In IFEs Call
-    (async () => {
-      const moreData = await fetch(
-        API_URL + "lat=12.9351929&lng=77.62448069999999"
-      );
-      const moreDataRes = await moreData.json();
-      const moreDataCard =
-        moreDataRes?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle
-          ?.restaurants;
-      console.log(moreDataRes)
-      setFilterRes([...filterRes, ...moreDataCard]);
-      setSrchRes([...srchRes, ...moreDataCard]);
-    })();
-  };
+  // const srchVal = () => {
+  //   const filterData = filterRes.filter((item) =>
+  //     item.info.name.toLowerCase().includes(inputVal.toLowerCase())
+  //   );
+  //   setSrchRes(filterData);
+  // };
+  console.log("i'm still in body");
+  // const fetchMoreBtn = async () => {
+  //   //* In IFEs Call
+  //   (async () => {
+  //     const moreData = await fetch(
+  //       API_URL + "lat=12.9351929&lng=77.62448069999999"
+  //     );
+  //     const moreDataRes = await moreData.json();
+  //     const moreDataCard =
+  //       moreDataRes?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle
+  //         ?.restaurants;
+  //     console.log(moreDataRes)
+  //     setFilterRes([...filterRes, ...moreDataCard]);
+  //     setSrchRes([...srchRes, ...moreDataCard]);
+  //   })();
+  // };
 
   //! Rendering
   //* condlitional rendering
-  // console.log(filterRes);
+  console.log(filterRes);
   return filterRes.length === 0 ? (
     <Skeleton />
   ) : (
     <div className="body">
-      <SrchArea
+      {/* <SrchArea
         inputVal={inputVal}
         setInputVal={setInputVal}
         srchVal={srchVal}
-      />
-      {srchRes.length === 0 && <NotFound />}
+      /> */}
+      {/* {srchRes.length === 0 && <NotFound />} */}
       <div className="con-wrap">
         <div className="res-container" style={{ width: containerWidth.width }}>
-          {srchRes.map((item) => (
+          {filterRes.map((item) => (
             <Link to={`/resturant/${item.info.id}`} key={item.info.id}>
               <InfoCard resData={item.info} />
             </Link>
           ))}
         </div>
-        {srchRes.length !== 0 && inputVal.length === 0 && (
+        {/* {srchRes.length !== 0 && inputVal.length === 0 && (
           <span className="show-more" onClick={fetchMoreBtn}>
             SHOW MORE {">"}
           </span>
-        )}
+        )} */}
       </div>
     </div>
   );
