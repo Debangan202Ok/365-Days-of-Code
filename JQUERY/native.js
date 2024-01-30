01748bcd0b3288c1f2552a1285028f3273c9abf9
paul <98002712+Debangan202Ok@users.noreply.github.com>
2024-01-30 11:32:08 +0530

    os module use

--- JQUERY/native.js
+++ JQUERY/native.js
@@ -1,7 +1,19 @@
-const fs = require('node:fs');
-const os = require('node:os')
+const fs = require("node:fs");
+const os = require("node:os");
 
 // console.log(fs)
 fs.writeFile("intro.txt", "hello world !", (err) => {
   if (err) console.log(err);
 });
+
+const user = os.hostname();
+const cpu = os.cpus();
+const cpuArch = os.arch();
+
+console.log("Host Name:", user);
+console.log(cpu, "architecture:", cpuArch);
+
+console.log(Math.floor(os.freemem() / (1024 * 1024 * 1024)), "gb is free");
+
+console.log(os.homedir());
+console.log(os.machine());
